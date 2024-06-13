34f7570c457329e629947d71c46ac05dd33db780
NISHALINI <nishalinithirukkumar2@gmail.com>
2024-06-13 12:35:05 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,24 @@
+
+
+let allMusic = [
+  {
+    name: "Appadi Podu",
+    artist: "Sriram",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kokkarakko",
+    artist: "Udit Narayan , Sujatha Mohan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Sha La La",
+    artist: "Sunidhi Chauhan",
+    img: "music-4",
+    src: "music-4"
+  },
+ 
+  
+];
